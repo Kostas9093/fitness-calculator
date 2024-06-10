69daa47b7c35c645823881030a0ec12998bbcc65
Kostas9093 <159778628+Kostas9093@users.noreply.github.com>
2024-06-10 20:50:01 +0100

    colors

--- src/components/ProgressDisplay.jsx
+++ src/components/ProgressDisplay.jsx
@@ -22,15 +22,14 @@ const ProgressDisplay = ({ progress }) => {
     <div className="new">
       <h2>Progress Results</h2>
       <p>Your weight has Change by: {weightDiff} kg<br/><br/> Your fat Mass Change by: {fatDiff} % <br/><br/>Your muscle Mass Change by: {muscleDiff} %<br/><br/>Your Water has Change by: {waterDiff} %<br/><br/></p>
-      <h3>Your fat Mass in Kilograms</h3>
-      <p>Previous fat Mass: {oldFatKilos} kg<br/><br/>Current fat Mass: {newFatKilos} kg</p>
-      <h4>Your progress in fat kilograms:
-       {muscleKilosDiff > 0 ? ` You gained ${fatKilosDiff} kg of fat mass` : ` You lost ${Math.abs(fatKilosDiff)} kg of fat mass`}</h4>
-      <h3>Muscle mass in Kilograms</h3>
-      <p>Previous muscle Mass: {oldMuscleKilos} kg<br/><br/>Current muscle Mass: {newMuscleKilos} kg</p>
-      <h4>Your progress in muscle kilograms:
-       {muscleKilosDiff > 0 ? ` You gained ${muscleKilosDiff} kg of muscles mass` : ` You lost ${Math.abs(muscleKilosDiff)} kg of muscles mass`}
-      </h4>
+      {/* <h3>Your fat Mass in Kilograms</h3>
+      <p>Previous fat Mass: {oldFatKilos} kg<br/><br/>Current fat Mass: {newFatKilos} kg</p> */}<h3>
+      {muscleKilosDiff > 0 ?  `Carefull... :` : ` Congratulations!!!!`}</h3>
+      <h4>{muscleKilosDiff > 0 ? ` You gained ${fatKilosDiff} kg of fat` : ` You lost ${Math.abs(fatKilosDiff)} kg of fat`}</h4>
+      {/* <h3>Muscle mass in Kilograms</h3> */}
+      {/* <p>Previous muscle Mass: {oldMuscleKilos} kg<br/><br/>Current muscle Mass: {newMuscleKilos} kg</p> */}
+      <h3>{muscleKilosDiff > 0 ? ` Congratulations!!!!` : `Carefull... :`}</h3>
+      <h4>{muscleKilosDiff > 0 ? ` You gained ${muscleKilosDiff} kg of muscles mass` : ` You lost ${Math.abs(muscleKilosDiff)} kg of muscles mass`}</h4>
     </div>
   );
 };
