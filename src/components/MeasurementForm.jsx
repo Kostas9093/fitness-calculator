c9095cfcb97b55b0e3a669013640f9d88f53092b
Kostas9093 <159778628+Kostas9093@users.noreply.github.com>
2024-06-12 10:45:10 +0100

    GE

--- src/components/MeasurementForm.jsx
+++ src/components/MeasurementForm.jsx
@@ -1,86 +1,184 @@
-// src/components/MeasurementForm.js
 import React, { useState, useEffect } from 'react';
 
-const MeasurementForm = ({ onCalculate }) => {
-  const [measurements, setMeasurements] = useState({
-    oldWeight: '',
-    oldFat: '',
-    oldMuscle: '',
-    oldWater: '',
-    newWeight: '',
-    newFat: '',
-    newMuscle: '',
-    newWater: ''
-  });
+const ProgressDisplay = ({ progress }) => {
+  if (!progress) return null;
 
-  useEffect(() => {
-    const savedMeasurements = JSON.parse(localStorage.getItem('oldMeasurements'));
-    if (savedMeasurements) {
-      setMeasurements((prevMeasurements) => ({
-        ...prevMeasurements,
-        ...savedMeasurements
-      }));
-    }
-  }, []);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setMeasurements({
-      ...measurements,
-      [name]: value
-    });
-  };
-
-  
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const { oldWeight, oldFat, oldMuscle, oldWater, ...newMeasurements } = measurements;
-    const oldMeasurements = { oldWeight, oldFat, oldMuscle, oldWater };
-    localStorage.setItem('oldMeasurements', JSON.stringify(oldMeasurements));
-    onCalculate(measurements);
-  };
+  const {
+    weightDiff,
+    fatDiff,
+    muscleDiff,
+    waterDiff,
+    musleKilosDiff,
+    fatKilosDiff,
+  } = progress;
 
   return (
-    <form id="old" onSubmit={handleSubmit}>
-      <h2>Enter Your Previous Measurements</h2>
-      <label>
-        Previous Weight (kg):
-        <input type="number" placeholder="Enter Your Previous Weight in kg" name="oldWeight" value={measurements.oldWeight} onChange={handleChange} required />
-      </label>
-      <label>
-      Previous Fat Mass (%):
-        <input type="number" placeholder="Enter Your Previous Fat Mass in %"  name="oldFat" value={measurements.oldFat} onChange={handleChange} required />
-      </label>
-      <label>
-      Previous Muscle Mass (%):
-        <input type="number" placeholder="Enter Your Previous Muscle Mass in %"  name="oldMuscle" value={measurements.oldMuscle} onChange={handleChange} required />
-      </label>
-      <label>
-      Previous Water (%):
-        <input type="number" placeholder="Enter Your Previous Water in %" name="oldWater" value={measurements.oldWater} onChange={handleChange} required />
-      </label>
-
-      <h2>Enter Your New Measurements</h2>
-      <label>
-        Current Weight (kg):
-        <input type="number" placeholder="Enter Your Current Weight in kg"  name="newWeight" value={measurements.newWeight} onChange={handleChange} required />
-      </label>
-      <label>
-      Current Fat Mass (%):
-        <input type="number" name="newFat" placeholder="Enter Your Current Fat Mass in %" value={measurements.newFat} onChange={handleChange} required />
-      </label>
-      <label>
-      Current Muscle Mass (%):
-        <input type="number" name="newMuscle" placeholder="Enter Your Current Muscle Mass %"  value={measurements.newMuscle} onChange={handleChange} required />
-      </label>
-      <label>
-      Current Water (%):
-        <input type="number" name="newWater" placeholder="Enter Your Current Water in %"  value={measurements.newWater} onChange={handleChange} required />
-      </label>
-
-      <button type="submit">Calculate Progress</button>
-    </form>
+    <div className="new">
+      <h2>Progress Results</h2>
+      <p>Weight Change: {weightDiff  >= 0 ? `You gained ${weightDiff} kg` : `You lost ${Math.abs(weightDiff).toFixed(2)} kg`}</p>
+      <p>Fat Mass Change: {fatDiff >= 0 ? `You gained ${fatDiff} %` : `You lost ${Math.abs(fatDiff).toFixed(2)} %`} </p>
+      <p>Muscle Mass Change: {muscleDiff >= 0 ? `You gained ${muscleDiff} %` : `You lost ${Math.abs(muscleDiff).toFixed(2)} %`} </p>
+      <p>Fat Kilos Change: {fatKilosDiff >= 0 ? `You gained ${fatKilosDiff} kg` : `You lost ${Math.abs(fatKilosDiff).toFixed(2)} kg`} </p>
+      <p>Muscle Kilos Change: {musleKilosDiff >= 0 ? `You gained ${musleKilosDiff} kg` : `You lost ${Math.abs(musleKilosDiff).toFixed(2)} kg`}</p>
+      <p>Water Change: {waterDiff} %</p>
+    </div>
   );
 };
 
-export default MeasurementForm;
+const MeasurementForm = () => {
+  const [oldMeasurements, setOldMeasurements] = useState({
+    weight: '',
+    fat: '',
+    muscle: '',
+    water: ''
+  });
+
+  const [newMeasurements, setNewMeasurements] = useState({
+    weight: '',
+    fat: '',
+    muscle: '',
+    water: ''
+  });
+
+  const [progress, setProgress] = useState(null);
+
+  useEffect(() => {
+    const savedOldMeasurements = JSON.parse(localStorage.getItem('oldMeasurements'));
+    if (savedOldMeasurements) {
+      setOldMeasurements(savedOldMeasurements);
+    }
+  }, []);
+
+  const handleChange = (e, type, isOld = true) => {
+    const { name, value } = e.target;
+    if (isOld) {
+      setOldMeasurements(prevState => ({
+        ...prevState,
+        [name]: value
+      }));
+    } else {
+      setNewMeasurements(prevState => ({
+        ...prevState,
+        [name]: value
+      }));
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const oldFatKilos = (oldMeasurements.weight * oldMeasurements.fat) / 100;
+    const newFatKilos = (newMeasurements.weight * newMeasurements.fat) / 100;
+    const oldMuscleKilos = (oldMeasurements.weight * oldMeasurements.muscle) / 100;
+    const newMuscleKilos = (newMeasurements.weight * newMeasurements.muscle) / 100;
+
+    const progress = {
+      weightDiff: newMeasurements.weight - oldMeasurements.weight,
+      fatDiff: newMeasurements.fat - oldMeasurements.fat,
+      muscleDiff: newMeasurements.muscle - oldMeasurements.muscle,
+      waterDiff: newMeasurements.water - oldMeasurements.water,
+      fatKilosDiff: newFatKilos - oldFatKilos,
+      musleKilosDiff: newMuscleKilos - oldMuscleKilos
+    };
+
+    setProgress(progress);
+
+    localStorage.setItem('oldMeasurements', JSON.stringify(newMeasurements));
+  };
+
+  return (
+    <div>
+     
+      <form onSubmit={handleSubmit}>
+        <h2>Old Measurements</h2>
+        <div>
+          <label>Weight (kg):</label>
+          <input
+            type="number"
+            placeholder="Enter Your Previous Weight in kg"
+            name="weight"
+            value={oldMeasurements.weight}
+            onChange={(e) => handleChange(e, 'weight')}
+          />
+        </div>
+        <div>
+          <label>Fat Mass (%):</label>
+          <input
+            type="number"
+            placeholder="Enter Your Previous Fat Mass in %"
+            name="fat"
+            value={oldMeasurements.fat}
+            onChange={(e) => handleChange(e, 'fat')}
+          />
+        </div>
+        <div>
+          <label>Muscle Mass (%):</label>
+          <input
+            type="number"
+            placeholder="Enter Your Previous Muscle Mass in %"
+            name="muscle"
+            value={oldMeasurements.muscle}
+            onChange={(e) => handleChange(e, 'muscle')}
+          />
+        </div>
+        <div>
+          <label>Water (%):</label>
+          <input
+            type="number"
+            placeholder="Enter Your Previous Water in %"
+            name="water"
+            value={oldMeasurements.water}
+            onChange={(e) => handleChange(e, 'water')}
+          />
+        </div>
+
+        <h2>New Measurements</h2>
+        <div>
+          <label>Weight (kg):</label>
+          <input
+            type="number"
+            placeholder="Enter Your Current Weight in kg"
+            name="weight"
+            value={newMeasurements.weight}
+            onChange={(e) => handleChange(e, 'weight', false)}
+          />
+        </div>
+        <div>
+          <label>Fat Mass (%):</label>
+          <input
+            type="number"
+            placeholder="Enter Your Current Fat Mass in %"
+            name="fat"
+            value={newMeasurements.fat}
+            onChange={(e) => handleChange(e, 'fat', false)}
+          />
+        </div>
+        <div>
+          <label>Muscle Mass (%):</label>
+          <input
+            type="number"
+            placeholder="Enter Your Current Muscle Mass %"
+            name="muscle"
+            value={newMeasurements.muscle}
+            onChange={(e) => handleChange(e, 'muscle', false)}
+          />
+        </div>
+        <div>
+          <label>Water (%):</label>
+          <input
+            type="number"
+            placeholder="Enter Your Current Water in %"
+            name="water"
+            value={newMeasurements.water}
+            onChange={(e) => handleChange(e, 'water', false)}
+          />
+        </div>
+        <button type="submit">Compare</button>
+      </form>
+
+      <ProgressDisplay progress={progress} />
+    </div>
+  );
+};
+
+export default  MeasurementForm ;
--- src/components/ProgressDisplay.jsx
+++ src/components/ProgressDisplay.jsx
@@ -18,20 +18,7 @@ const ProgressDisplay = ({ progress }) => {
   const fatKilosDiff = (newFatKilos - oldFatKilos).toFixed(2);
   const muscleKilosDiff = (newMuscleKilos - oldMuscleKilos).toFixed(2);
 
-  return (
-    <div className="new">
-      <h2>Progress Results</h2>
-      <p>Your weight has Change by: {weightDiff} kg<br/><br/> Your fat Mass Change by: {fatDiff} % <br/><br/>Your muscle Mass Change by: {muscleDiff} %<br/><br/>Your Water has Change by: {waterDiff} %<br/><br/></p>
-      {/* <h3>Your fat Mass in Kilograms</h3>
-      <p>Previous fat Mass: {oldFatKilos} kg<br/><br/>Current fat Mass: {newFatKilos} kg</p> */}<h3>
-      {muscleKilosDiff > 0 ?  `Congratulations!!!!` : `Carefull...`}</h3>
-      <h4>{muscleKilosDiff > 0 ? ` You lost ${fatKilosDiff} kg of fat` : `You gained  ${Math.abs (fatKilosDiff)} kg of fat`}</h4>
-      {/* <h3>Muscle mass in Kilograms</h3> */}
-      {/* <p>Previous muscle Mass: {oldMuscleKilos} kg<br/><br/>Current muscle Mass: {newMuscleKilos} kg</p> */}
-      <h3>{muscleKilosDiff > 0 ? ` Congratulations!!!!` : `Carefull... :`}</h3>
-      <h4>{muscleKilosDiff > 0 ? ` You gained ${muscleKilosDiff} kg of muscles mass` : ` You lost ${Math.abs(muscleKilosDiff)} kg of muscles mass`}</h4>
-    </div>
-  );
+  return (<></>);
 };
 
 export default ProgressDisplay;
